5188760ddcd26126932309fcbd25a454ee774800
afif.patel1 <afif.patel1@loblaw.ca>
2026-04-13 23:58:28 +0500

    new changes

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/hello", (req, res, next) => {
+  res.send(
+    "<form action='/submit' method='post'><input type='text' name='name'><button type='submit'>Submit</button></form>",
+  );
+  next();
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("Form submitted");
+});
+
 app.use((req, res, next) => {
   console.log("Request received");
   next();
